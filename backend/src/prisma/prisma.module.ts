c171c504e2f9392c7d8c980bca32916f714b530a
Deniz Suludere <suludere.1996@gmail.com>
2026-03-10 13:12:12 +0100

    fixed render.com issues 2.0

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { PrismaService } from './prisma.service';
 
 @Module({
