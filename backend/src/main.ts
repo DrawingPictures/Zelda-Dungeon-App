fddde83ab9159800d0d8589f9507f1cafdcdd6c4
Deniz Suludere <suludere.1996@gmail.com>
2026-03-10 12:16:27 +0100

    fix: database connection and vercel deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://192.168.56.1:3000",
+      "https://zeldadungeonapp.vercel.app"
+      ],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, "0.0.0.0");
 }
 bootstrap();
